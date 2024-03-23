c2f8e9c44fad95b9eb672d12b0c2ee3245bac024
logonrmlocal <logonrmlocal@L2801MICRO14.fiap.com.br>
2024-03-22 22:24:08 -0300

    Exercicios com While, Loop e For

--- OracleFiap.sql
+++ OracleFiap.sql
@@ -2,7 +2,10 @@ set SERVEROUTPUT on;
 
 
 DECLARE
-    v_contador number(2):=0;
+    v_contador number(2):=1;
+    num_par number := 0;
+    num_impar number := 0;
+    
 /*
 begin 
 loop
@@ -25,7 +28,6 @@ begin
    dbms_output.put_line(v_contador);
 end loop;
 end;
-*/
 
 
 begin 
@@ -33,4 +35,39 @@ begin
     dbms_output.put_line(v_contador * 150);
 end loop;
 end;
+
+
+
+begin 
+    for v_contador in 1..10 loop
+        if mod(v_contador, 2)= 0 THEN
+            dbms_output.put_line('Numero par!! ' || v_contador);
+        else
+            dbms_output.put_line('Numero impar!! ' || v_contador);
+        end if;
+    end loop;
+end;
+
+
+
+begin
+    for v_contador in 1..100 loop
+        if mod(v_contador, 2) = 0 then
+            num_par := num_par + 1;
+        else
+            num_impar := num_impar + 1;
+        end if;
+    end loop;
+    dbms_output.put_line('Numeros pare: ' || num_par);
+    dbms_output.put_line('Numeros impares: ' || num_impar);
+end;
+
+
+begin
+    while v_contador<=20 loop
+        dbms_output.put_line(v_contador);
+        v_contador := v_contador + 1;
+    end loop;
+end;
+*/
 
